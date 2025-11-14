e380404e50138b34c9461cfbffab38b76a95a258
LuisMiguel123mkdir <127710387+LuisMiguel123mkdir@users.noreply.github.com>
2025-11-14 12:45:56 -0500

    Update login.js

--- login.js
+++ login.js
@@ -1,10 +1,6 @@
 const supabaseUrl = 'https://wnwlqvhqnumualecxnuh.supabase.co'; 
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indud2xxdmhxbnVtdWFsZWN4bnVoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMwNDM0NTcsImV4cCI6MjA3ODYxOTQ1N30.-fPjOVaAGvVGfCUSVMtu9yLuxxJL02oMQ-qtxpP8CtE'; 
-const supabase = window.supabase.createClient(supabaseUrl, supabaseKey, {
-  auth: {
-    persistSession: false
-  }
-});
+const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
 
 document.getElementById('formLogin').addEventListener('submit', async (e) => {
   e.preventDefault();

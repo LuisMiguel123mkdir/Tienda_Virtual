910bd7821eba33d6396cf25ce0978af9a7b2fd5a
LuisMiguel123mkdir <127710387+LuisMiguel123mkdir@users.noreply.github.com>
2025-11-14 11:02:35 -0500

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-// 1. Conexión a Supabase 
-
-const supabaseUrl = 'https://wnwlqvhqnumualecxnuh.supabase.co'; 
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indud2xxdmhxbnVtdWFsZWN4bnVoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMwNDM0NTcsImV4cCI6MjA3ODYxOTQ1N30.-fPjOVaAGvVGfCUSVMtu9yLuxxJL02oMQ-qtxpP8CtE'; 
-const supabase = window.supabase.createClient(supabaseUrl, supabaseKey, {
-  auth: {
-    persistSession: false
-  }
-});

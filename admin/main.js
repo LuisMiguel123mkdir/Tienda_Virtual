932acdb15631e9ea3396a808def1cb57a925be2e
LuisMiguel123mkdir <127710387+LuisMiguel123mkdir@users.noreply.github.com>
2025-11-17 13:42:13 -0500

    Update main.js

--- admin/main.js
+++ admin/main.js
@@ -2,7 +2,7 @@
 
 const supabaseUrl = 'https://wnwlqvhqnumualecxnuh.supabase.co'; 
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indud2xxdmhxbnVtdWFsZWN4bnVoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMwNDM0NTcsImV4cCI6MjA3ODYxOTQ1N30.-fPjOVaAGvVGfCUSVMtu9yLuxxJL02oMQ-qtxpP8CtE'; 
-const supabase = supabase.createClient(supabaseUrl, supabaseKey, {
+const supabase = window.supabase.createClient(supabaseUrl, supabaseKey, {
   auth: { persistSession: false }
 });
 
@@ -349,3 +349,4 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   }
 });
+

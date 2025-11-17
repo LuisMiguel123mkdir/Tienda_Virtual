8f45e20332dc50b6860e702da1c2fe0df96c4551
LuisMiguel123mkdir <127710387+LuisMiguel123mkdir@users.noreply.github.com>
2025-11-17 11:27:45 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 // Conexión a Supabase
 const supabaseUrl = 'https://wnwlqvhqnumualecxnuh.supabase.co'; 
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indud2xxdmhxbnVtdWFsZWN4bnVoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMwNDM0NTcsImV4cCI6MjA3ODYxOTQ1N30.-fPjOVaAGvVGfCUSVMtu9yLuxxJL02oMQ-qtxpP8CtE'; 
-const supabase = supabase.createClient(supabaseUrl, supabaseKey);
-
+const supabase = supabase.createClient(supabaseUrl, supabaseKey, {
+  auth: {
+    persistSession: false
+  }
+});
 
 let productosGlobal = []; // almacenará todos los productos
 
